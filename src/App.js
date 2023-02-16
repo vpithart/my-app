56c1046186d3365512d1eeb1c741720d93197795
Vojtech Pithart <vojtech.pithart@onevest.com>
2023-02-16 13:26:31 -0700

    go "c"

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Lorem. ipsum.
+          Lorem ipsum dolor
         </p>
         <a
           className="App-link"
